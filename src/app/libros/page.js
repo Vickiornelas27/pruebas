263a5aa2e03c83d7dc20cd5cc61d007a7872b769
Vickiornelas27 <102771422+Vickiornelas27@users.noreply.github.com>
2023-05-21 10:59:27 -0700

    Navbar modificado
    Se cambio el navbar por un diseño de next ui

--- src/app/libros/page.js
+++ src/app/libros/page.js
@@ -1,45 +1,10 @@
 import cardList from '../data';
 
-import React, { Component } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
 
-const Libros  = () => {
+const Libros = () => {
   return (
-<<<<<<< HEAD
     <>
- <div>
-              <h2>NextJs Carousel - GeeksforGeeks</h2>
-              <Carousel>
-                  <div>
-                      <img src="/1.png" alt="image1"/>
-                      <p className="legend">Image 1</p>
-  
-                  </div>
-                  <div>
-                      <img src="/2.png" alt="image2" />
-                      <p className="legend">Image 2</p>
-  
-                  </div>
-                  <div>
-                      <img src="/3.png" alt="image3"/>
-                      <p className="legend">Image 3</p>
-  
-                  </div>
-                  <div>
-                      <img src="/4.png" alt="image4"/>
-                      <p className="legend">Image 4</p>
-  
-                  </div>
-                  <div>
-                      <img src="/5.png" alt="image5"/>
-                      <p className="legend">Image 5</p>
-  
-                  </div>
-              </Carousel>
-</div>
-    
-        {/*  <h1 className='font-bold text-5xl text-center my-5'>Nuestra colección</h1>
+        <h1 className='font-bold text-5xl text-center my-5'>Nuestra colección</h1>
         <div className='mx-auto'>
         <h1 className='font-bold text-3xl text-center'>Drama</h1>      
         <div className='grid lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-5  gap-6 align-middle justify-items-center m-10 '>
@@ -84,26 +49,9 @@ const Libros  = () => {
             ))}
         </div>
                 
-        </div>  */}
-=======
-    <><h1 className='font-bold text-5xl text-center my-5'>Nuestra colección</h1>
-    <div className='mx-auto'>
-      <div className='grid lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-3 xl:grid-rows-2 gap-6 align-middle justify-items-center m-10'>
-        {cardList.map((card) => (
-          <div className='shadow-lg rounded-lg bg-white'>
-            <img className='rounded-t-lg' src={card.img} alt='' width='640px' height='853px' />
-            <div className='p-5' width='50'>
-              <h3 className='text 3xl font-bold mb-3'>{card.title}</h3>
-              <p className='text-lg font-normal'>{card.text}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-    
->>>>>>> 4ccf2308424679d432fc3a12d299d6717cbedb62
+        </div>
   </>
 
   )
 }
-export default Libros 
+export default Libros
